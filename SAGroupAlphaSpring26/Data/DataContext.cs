63ca484feeaeb94cada7eb4c824dbbddc12ab215
evang8 <evang8@students.ntc.edu>
2026-02-16 20:45:05 -0600

    Made some local test migrations was able to successfully make a PieceTypes table (LOCALLY)

---
+++ SAGroupAlphaSpring26/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SAGroupAlphaSpring26.Data
+{
+    // The data context class.
+    // serves as a bridge between your domain model (your C# classes) and the underlying database. 
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        { }
+
+        // So this is a reference to a PieceTypes table.
+        /*
+        * property in your DbContext class generally corresponds to a table in the database, where TEntity is the C# class (model) that maps to the table's structure.
+        */
+        public DbSet<PieceType> PieceTypes { get; set; }
+    }
+}
--- SAGroupAlphaSpring26/Program.cs
+++ SAGroupAlphaSpring26/Program.cs
@@ -1,5 +1,7 @@
 global using SAGroupAlphaSpring26;
 global using SAGroupAlphaSpring26.Models;
+using Microsoft.EntityFrameworkCore;
+using SAGroupAlphaSpring26.Data;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.Configure<AppConfig>(builder.Configuration.GetSection("AppConfig"));
+
+// Adding the database for the EF core, and connecting it to the connection string in the appsettings.json file.
+// This is the DB that our C# classes will be stored in, and we will use EF core to interact with it.
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
