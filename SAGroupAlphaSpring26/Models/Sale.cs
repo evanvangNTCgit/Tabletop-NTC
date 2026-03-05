1b14bfe5826ff308bbf774a0f61906edb8786b9a
Evan Vang <evang8@students.ntc.edu>
2026-03-05 11:16:59 -0600

    Sale and sale line models added. Got rid of others that were scrapped from the ERD.

---
+++ SAGroupAlphaSpring26/Models/Sale.cs
@@ -0,0 +1,20 @@
+﻿namespace SAGroupAlphaSpring26.Models
+{
+    public class Sale
+    {
+        // The ID Of the sale from user.
+        public int SaleID { get; set; }
+
+        // The ID of the user on the sale.
+        public int UserID { get; set; }
+
+        // The navigation property to the user.
+        public required User User { get; set; }
+
+        // The date of the sale.
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        // The total price of the sale... maybe just a LINQ QUERY somewhere down the line?
+        // Nothing right now just think of this later down the line.
+    }
+}
---
+++ SAGroupAlphaSpring26/Models/SaleLine.cs
@@ -0,0 +1,23 @@
+﻿namespace SAGroupAlphaSpring26.Models
+{
+    public class SaleLine
+    {
+        // The ID of the saleline. 
+        public required int SaleLineID { get; set; }
+
+        // Since a saleline can be apart of the same transaction (so a set of 10 skeletons was bought), we need to link salelines to a single Sale. 
+        public int SaleID { get; set; }
+
+        // Navigation property to the sale.
+        // public Sale Sale {get; set; }
+
+        // The ID of the piece sold in this line.
+        public int PieceID { get; set; }
+
+        // The navigation property to the piece sold in this line.
+        public required Piece Piece { get; set; }
+
+        // The price of the sale line.
+        public decimal Price { get; set; }
+    }
+}
