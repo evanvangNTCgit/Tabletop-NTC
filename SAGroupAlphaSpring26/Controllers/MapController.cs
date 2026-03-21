773ae6b130c5b0057f112d1d432b7e60a8c12946
Evan Vang <evang8@students.ntc.edu>
2026-03-21 10:28:01 -0500

    Slight clean up

--- SAGroupAlphaSpring26/Controllers/MapController.cs
+++ SAGroupAlphaSpring26/Controllers/MapController.cs
@@ -62,26 +62,6 @@ namespace SAGroupAlphaSpring26.Controllers
         //    return Json(tokens);
         //}
 
-
-        // Helper class for token JSON data.
-        public class TokenUpdateModel
-        {
-            public int Id { get; set; }
-            public double X { get; set; }
-            public double Y { get; set; }
-            public int zIndex { get; set; }
-            public int SessionID { get; set; }
-            public string Name { get; set; } = string.Empty;
-        }
-
-        public class TokenCreateModel
-        {
-            public int PieceId { get; set; }
-            public int SessionId { get; set; }
-            public double X { get; set; }
-            public double Y { get; set; }
-        }
-
         [HttpPost]
         public IActionResult CreateToken([FromBody] TokenCreateModel model)
         {
