a456fefdb482b686b14ebbe083e33cda1f8f6a38
Tristan_Jackson <tjackson@students.ntc.edu>
2026-04-28 11:17:50 -0500

    PBI 1839: Session Notes panel within map view is now functional. Added a button to toggle session notes, as well as a button to toggle token info. Session notes save(writes to DB) using either save button.

---
+++ SAGroupAlphaSpring26/Models/SessionNotesUpdateModel.cs
@@ -0,0 +1,8 @@
+namespace SAGroupAlphaSpring26.Models
+{
+    public class SessionNotesUpdateModel
+    {
+        public int SessionId { get; set; }
+        public string Notes { get; set; } = string.Empty;
+    }
+}
