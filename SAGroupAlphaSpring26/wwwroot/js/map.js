105d1468ec8e8e20ae5f3e65a37a0ee3fd318330
Evan Vang <evang8@students.ntc.edu>
2026-04-30 11:53:03 -0500

    Player view tokens share same styling as DM view.

--- SAGroupAlphaSpring26/wwwroot/js/map.js
+++ SAGroupAlphaSpring26/wwwroot/js/map.js
@@ -218,6 +218,9 @@ if (!isPlayerView) {
         tokenImg.src = data.src;
 
         tokenImg.classList.add('draggable-token');
+        tokenImg.classList.add('ui-draggable');
+        tokenImg.classList.add('ui-draggable-handle');
+        tokenImg.classList.add('map-piece');
         tokenImg.dataset.tokenid = data.id;
         tokenImg.dataset.pieceid = data.pieceId;
         tokenImg.draggable = true;
